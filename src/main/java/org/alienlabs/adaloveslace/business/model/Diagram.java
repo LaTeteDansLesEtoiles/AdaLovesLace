21ab87fcde43960a4a46abe72e4a2b24632304e4
Alien@lv426 <goupilpierre@gmail.com>
2022-03-10 23:39:15 +0100

    1-basicdrawing
    ADDED more of model

---
+++ src/main/java/org/alienlabs/adaloveslace/business/model/Diagram.java
@@ -0,0 +1,44 @@
+package org.alienlabs.adaloveslace.business.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Diagram {
+
+  private final List<Pattern> patterns;
+  private final List<Knot>    knots;
+
+  public Diagram() {
+    this.patterns = new ArrayList<>();
+    this.knots    = new ArrayList<>();
+  }
+
+  public List<Pattern> getPatterns() {
+    return this.patterns;
+  }
+
+  public List<Knot> getKnots() {
+    return this.knots;
+  }
+
+  public List<Pattern> addPattern(final Pattern pattern) {
+    this.patterns.add(pattern);
+    return this.patterns;
+  }
+
+  public List<Knot> addKnot(final Knot knot) {
+    this.knots.add(knot);
+    return this.knots;
+  }
+
+  public List<Pattern> clearPatterns() {
+    this.patterns.clear();
+    return this.patterns;
+  }
+
+  public List<Knot> clearKnots() {
+    this.knots.clear();
+    return this.knots;
+  }
+
+}
