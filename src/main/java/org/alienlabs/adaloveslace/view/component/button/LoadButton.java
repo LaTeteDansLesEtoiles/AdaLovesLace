38f9339ddefe301715791062a0e18564de75a56a
Alien@lv426 <goupilpierre@gmail.com>
2022-04-22 14:15:04 +0200

    ADDED load file from a folder in a JavaFX preference if we can

--- src/main/java/org/alienlabs/adaloveslace/view/component/button/LoadButton.java
+++ src/main/java/org/alienlabs/adaloveslace/view/component/button/LoadButton.java
@@ -8,10 +8,14 @@ import javafx.scene.layout.Pane;
 import javafx.stage.FileChooser;
 import org.alienlabs.adaloveslace.App;
 import org.alienlabs.adaloveslace.business.model.Diagram;
+import org.alienlabs.adaloveslace.util.Preferences;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+
+import static org.alienlabs.adaloveslace.util.Preferences.SAVED_XML_FILE;
+import static org.alienlabs.adaloveslace.util.Preferences.XML_FILE_FOLDER_SAVE_PATH;
 
 public class LoadButton extends Button {
 
@@ -33,7 +37,18 @@ public class LoadButton extends Button {
 
     FileChooser load = new FileChooser();
     load.setTitle(LOAD_FILE_DIALOG_TITLE);
-    load.setInitialDirectory(new File(System.getProperty("user.home")));
+
+    Preferences preferences = new Preferences();
+    File xmlFile      = preferences.getPathWithFileValue(SAVED_XML_FILE);
+    File xmlFilePath  = preferences.getPathWithFileValue(XML_FILE_FOLDER_SAVE_PATH);
+
+    if (xmlFilePath == null || !xmlFilePath.canRead() || !xmlFile.canRead()) {
+      // We don't know from where to load
+      load.setInitialDirectory(new File(System.getProperty("user.home")));
+    } else {
+      // We do know
+      load.setInitialDirectory(xmlFilePath);
+    }
 
     FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter(DIAGRAM_FILES, DIAGRAM_FILE_FILTER);
     load.getExtensionFilters().add(filter);
