0efe231d55e96227c61af388f9037b60d3226ece
Alien@lv426 <goupilpierre@gmail.com>
2022-10-15 02:46:30 +0200

    23-frenchuserguide => small FIX for delivery packages

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,19 +2,18 @@ module org.alienlabs.adaloveslace.impl {
   // iText
   requires io;
   requires kernel;
-  requires layout;
 
   requires com.google.gson;
   requires jakarta.xml.bind;
   requires java.net.http;
-  requires javafx.controls;
-  requires javafx.graphics;
-  requires javafx.swing;
   requires java.prefs;
   requires org.slf4j;
   requires static com.github.spotbugs.annotations;
+  requires static javafx.controls;
+  requires static javafx.swing;
+  requires static layout;
 
-  opens org.alienlabs.adaloveslace                  to javafx.graphics, javafx.controls;
+  opens org.alienlabs.adaloveslace                  to javafx.controls, javafx.graphics, layout;
   opens org.alienlabs.adaloveslace.business.model   to com.google.gson, jakarta.xml.bind;
 
   exports org.alienlabs.adaloveslace;
