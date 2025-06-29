14a3c703bf845f5c98b109381f2f8fb31d5cc9e9
Alien@lv426 <goupilpierre@gmail.com>
2025-06-29 20:39:31 +0200

    119-fixtests => FIXED reset knots

--- src/main/java/org/alienlabs/adaloveslace/view/component/button/toolboxwindow/grid/ResetKnotsButton.java
+++ src/main/java/org/alienlabs/adaloveslace/view/component/button/toolboxwindow/grid/ResetKnotsButton.java
@@ -1,6 +1,8 @@
 package org.alienlabs.adaloveslace.view.component.button.toolboxwindow.grid;
 
 import javafx.event.EventHandler;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.input.MouseEvent;
 import org.alienlabs.adaloveslace.App;
@@ -9,6 +11,10 @@ import org.alienlabs.adaloveslace.util.ImageUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Optional;
+
+import static javafx.scene.control.Alert.AlertType.CONFIRMATION;
+import static javafx.scene.control.ButtonBar.ButtonData.CANCEL_CLOSE;
 import static org.alienlabs.adaloveslace.App.RESET_KNOTS_BUTTON_NAME;
 import static org.alienlabs.adaloveslace.App.resourceBundle;
 import static org.alienlabs.adaloveslace.view.window.ToolboxWindow.PATTERN_TEXT_AND_COLOR_BUTTON;
@@ -16,6 +22,12 @@ import static org.alienlabs.adaloveslace.view.window.ToolboxWindow.PATTERN_TEXT_
 public class ResetKnotsButton extends ToggleButton {
 
   private static App app;
+
+  private static final String RESET_KNOTS_WINDOW_TITLE        = "ResetKnotsWindowTitle";
+  private static final String RESET_KNOTS_HEADER_TEXT         = "ResetKnotsHeaderText";
+  private static final String RESET_KNOTS_CONTENT_TEXT        = "ResetKnotsContentText";
+  private static final String RESET_KNOTS_BUTTON_TEXT         = "ResetKnotsButtonText";
+  private static final String CANCEL_RESET_KNOTS_BUTTON_TEXT  = "CancelResetKnots";
 
   private static final Logger logger                  = LoggerFactory.getLogger(ResetKnotsButton.class);
 
@@ -31,13 +43,26 @@ public class ResetKnotsButton extends ToggleButton {
   }
 
   private final EventHandler<MouseEvent> onResetKnotsButtonClicked = _ -> {
-    app.unselectPatternsAndTextButtons();
-    app.getMovablePane().setOnKeyPressed(null);
+    Alert alert = new Alert(CONFIRMATION);
+    alert.setTitle(resourceBundle.getString(RESET_KNOTS_WINDOW_TITLE));
+    alert.setHeaderText(resourceBundle.getString(RESET_KNOTS_HEADER_TEXT));
+    alert.setContentText(resourceBundle.getString(RESET_KNOTS_CONTENT_TEXT));
 
-    new ImageUtil(app).backupKnots();
-    app.getOptionalDotGrid().getDiagram().getPatterns().clear();
-    app.getOptionalDotGrid().getDiagram().resetDiagram(app);
-    new FileChooserUtil().restartGui(app);
+    ButtonType resetKnotsButton  = new ButtonType(resourceBundle.getString(RESET_KNOTS_BUTTON_TEXT));
+    ButtonType cancelButton = new ButtonType(resourceBundle.getString(CANCEL_RESET_KNOTS_BUTTON_TEXT), CANCEL_CLOSE);
+
+    alert.getButtonTypes().setAll(resetKnotsButton, cancelButton);
+    Optional<ButtonType> result = alert.showAndWait();
+
+    if (result.isPresent() && result.get() == resetKnotsButton) {
+      app.unselectPatternsAndTextButtons();
+      app.getMovablePane().setOnKeyPressed(null);
+
+      new ImageUtil(app).backupKnots();
+      app.getOptionalDotGrid().getDiagram().getPatterns().clear();
+      app.getOptionalDotGrid().getDiagram().resetDiagram(app);
+      new FileChooserUtil().restartGui(app);
+    }
   };
 
 }
