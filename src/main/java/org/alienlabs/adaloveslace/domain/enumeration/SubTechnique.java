7247b5eb404288e9084c6db6964b8fd11a08f9c6
Alien@lv426 <goupilpierre@gmail.com>
2025-06-30 00:49:10 +0200

    119-fixtests => ADDED link from Technique to SubTechnique

--- src/main/java/org/alienlabs/adaloveslace/domain/enumeration/SubTechnique.java
+++ src/main/java/org/alienlabs/adaloveslace/domain/enumeration/SubTechnique.java
@@ -5,16 +5,22 @@ package org.alienlabs.adaloveslace.domain.enumeration;
  */
 public enum SubTechnique {
     CROCHET("crochet"),
+    MISC_CROCHET("crochet divers"),
     TATTING_LACE("dentelle à l&#39;aiguille"),
     SHETTLAND("dentelle de Shettland"),
+    MISC_LACE("dentelles diverses"),
     GRANNY_SQUARE("granny square"),
     CORNER_TO_CORNER("corner to corner"),
     MOSAIC_CROCHET("crochet mosa&#239;que"),
     DIAMOND_PAINTING("diamond painting"),
     PIXEL_ART("pixel-art"),
     CROSS_STITCH("point de croix"),
+    MISC_EMBROIDERY("broderies diverses"),
     TAPESTRY("tapisserie"),
-    PEYOTE_WEAVING("tissage peyote");
+    PEYOTE_WEAVING("tissage peyote"),
+    MISC_MISCELLANEOUS("divers"),
+    KNITTING("tricot"),
+    MISC_KNITTING("tricots divers");
 
     private final String value;
 
--- src/main/java/org/alienlabs/adaloveslace/domain/enumeration/Technique.java
+++ src/main/java/org/alienlabs/adaloveslace/domain/enumeration/Technique.java
@@ -1,22 +1,51 @@
 package org.alienlabs.adaloveslace.domain.enumeration;
 
+import java.util.List;
+
 /**
  * The Technique enumeration.
  */
 public enum Technique {
-    LACE("dentelle"),
-    KNITTING("tricot"),
-    CROCHET("crochet"),
-    EMBROIDERY("broderie"),
-    MISCELLANEOUS("divers");
+    LACE("dentelle",
+            SubTechnique.TATTING_LACE,
+            SubTechnique.MISC_LACE
+    ),
+    KNITTING("tricot",
+            SubTechnique.KNITTING,
+            SubTechnique.SHETTLAND,
+            SubTechnique.MISC_KNITTING
+    ),
+    CROCHET("crochet",
+            SubTechnique.CORNER_TO_CORNER,
+            SubTechnique.CROCHET,
+            SubTechnique.MOSAIC_CROCHET,
+            SubTechnique.TAPESTRY,
+            SubTechnique.MISC_CROCHET
+    ),
+    EMBROIDERY("broderie",
+            SubTechnique.CROSS_STITCH,
+            SubTechnique.MISC_EMBROIDERY
+    ),
+    MISCELLANEOUS("divers",
+            SubTechnique.DIAMOND_PAINTING,
+            SubTechnique.PIXEL_ART,
+            SubTechnique.PEYOTE_WEAVING,
+            SubTechnique.MISC_MISCELLANEOUS
+    );
 
     private final String value;
+    private final List<SubTechnique> subTechniques;
 
-    Technique(String value) {
+    Technique(String value, SubTechnique... subTechniquess) {
         this.value = value;
+        this.subTechniques = List.of(subTechniquess);
     }
 
     public String getValue() {
-        return value;
+        return this.value;
+    }
+
+    public List<SubTechnique> getSubTechniques() {
+        return this.subTechniques;
     }
 }
