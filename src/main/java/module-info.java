4c356b99cb0b437e363476272e68aad36e2bb7d2
Alien@lv426 <goupilpierre@gmail.com>
2026-03-20 18:11:29 +0100

    110-reorganizelayout: fix tests configuration

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -35,7 +35,7 @@ module org.alienlabs.adaloveslace {
   opens org.alienlabs.adaloveslace.domain.xmladapter  to com.google.gson, jakarta.xml.bind;
   exports org.alienlabs.adaloveslace.domain.dto;
   opens org.alienlabs.adaloveslace.domain.dto         to com.google.gson, jakarta.xml.bind;
-  opens org.alienlabs.adaloveslace.persistence.protobuf to protobuf.java;
+  opens org.alienlabs.adaloveslace.persistence        to com.google.protobuf;
   exports org.alienlabs.adaloveslace.view.component.button.toolboxwindow.file;
   exports org.alienlabs.adaloveslace.view.component.button.toolboxwindow.grid;
   exports org.alienlabs.adaloveslace.view.component.grid;
@@ -44,4 +44,5 @@ module org.alienlabs.adaloveslace {
   exports org.alienlabs.adaloveslace.view.window.event;
   exports org.alienlabs.adaloveslace.persistence;
   opens org.alienlabs.adaloveslace.view.window.event;
+  exports org.alienlabs.adaloveslace.persistence.protobuf;
 }
